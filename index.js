91919dc70d4f6774c1009a2952dac409f9fb550f
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-01-31 07:41:11 -0500

    commit tree 123a5cc2f1833f96940fc9ac5129c0030799b159

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(firstName, email) {
+    const configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': "application/json",
+            'Accept': "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    }
+  return fetch('http://localhost:3000/users', configObj)
+        .then(res => res.json())
+        .then(body => addNewContact(body.id))
+
+        .catch(function(error) {
+            alert("Unauthorized Access");
+            document.body.append(error);
+        })
+}
+function addNewContact(newID) {
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+    document.body.append(message)
+}
+submitData("Name", "name@name.com")
+
+
+
